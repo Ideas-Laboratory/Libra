ebe534b7cacb99297006def320869603284e1899
viruspc <pc479684801@outlook.com>
2021-07-12 22:27:17 +0800

    move setDragCommands and dragTool from augment-brush-link to dnm

--- src/demo/dust-n-magnet/scripts/setDragCommands.js
+++ src/demo/dust-n-magnet/scripts/setDragCommands.js
@@ -1,24 +1,35 @@
 import { getBackground } from "./helper";
 import IG from "~/IG";
+import * as d3 from "d3";
 
 /**
  * 通过setSharedScale暴露出个对象：offset: [dx, dy]
  * @param {} layer
  * @param {*} dragTool
  */
-let _pre = [0, 0];
-function setDrawRectXCommands(layer, dragTool) {
+let _pre = null;
+function setDragCommands(layer, dragTool) {
   layer.listen({
     tool: dragTool,
     startCommand: function (_, e) {
-      _pre = [e.x, e.y];
-      this.setSharedScale("offset", [0, 0]);
+      console.log("start command executed");
+      _pre = null;
+      // _pre = [e.x, e.y];
+      // this.setSharedScale("offset", [0, 0]);
+      // console.log("dragTool start");
     },
     dragCommand: function (_, e) {
-      const offset = [e.x - pre[0], e.y - pre[1]];
+      let offset;
+      const pos = d3.pointer(e.rawEvent, document);
+      if(_pre){
+        offset = [pos[0] - _pre[0], pos[1] - _pre[1]];
+      } else {
+        offset = [0, 0];
+      }
+      _pre = [pos[0], pos[1]];
       this.setSharedScale("offset", offset);
     },
   });
 }
 
-export default setDrawRectXCommands;
+export default setDragCommands;
